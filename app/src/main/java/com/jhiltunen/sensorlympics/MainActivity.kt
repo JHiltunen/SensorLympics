308bd0ec5c1fa9891b2f980569f36a8003923a07
keenanjon <jon.menna@metropolia.fi>
2022-02-11 11:39:46 +0200

    added viewmodel and sensorstuff

--- app/src/main/java/com/jhiltunen/sensorlympics/MainActivity.kt
+++ app/src/main/java/com/jhiltunen/sensorlympics/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.jhiltunen.sensorlympics
 
+import SensorViewModel
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
@@ -8,13 +14,36 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import chooseDirection
 import com.jhiltunen.sensorlympics.navigator.MainAppNav
 import com.jhiltunen.sensorlympics.ui.theme.SensorLympicsTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), SensorEventListener {
     @ExperimentalFoundationApi
+
+    companion object {
+        val sensorViewModel = SensorViewModel()
+    }
+
+    lateinit var sensorManager: SensorManager
+    lateinit var accelerometer: Sensor
+    lateinit var magnetometer: Sensor
+
+    var currentDegree = 0.0f
+    var lastAccelerometer = FloatArray(3)
+    var lastMagnetometer = FloatArray(3)
+    var lastAccelerometerSet = false
+    var lastMagnetometerSet = false
+
+    @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+        sensorViewModel.upDateWin(0)
+        chooseDirection()
         setContent {
             SensorLympicsTheme {
                 // A surface container using the 'background' color from the theme
@@ -25,6 +54,59 @@ class MainActivity : ComponentActivity() {
                     MainAppNav()
                 }
             }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST)
+        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_FASTEST)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this, accelerometer)
+        sensorManager.unregisterListener(this, magnetometer)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor === accelerometer) {
+            lowPass(event.values, lastAccelerometer)
+            lastAccelerometerSet = true
+        } else if (event.sensor === magnetometer) {
+            sensorViewModel.updateValue(
+                getString(
+                    R.string.sensor_val,
+                    event.values[0],
+                    event.values[1],
+                    event.values[2]
+                ), event.values[0], event.values[1]
+            )
+            lowPass(event.values, lastMagnetometer)
+            lastMagnetometerSet = true
+        }
+
+        if (lastAccelerometerSet && lastMagnetometerSet) {
+            val r = FloatArray(9)
+            if (SensorManager.getRotationMatrix(r, null, lastAccelerometer, lastMagnetometer)) {
+                val orientation = FloatArray(3)
+                SensorManager.getOrientation(r, orientation)
+                val degree = (Math.toDegrees(orientation[0].toDouble()) + 360).toFloat() % 360
+                Log.i("JOO", "${degree}")
+                currentDegree = degree
+                sensorViewModel.upDateDegree(currentDegree)
+
+            }
+        }
+    }
+
+    fun lowPass(input: FloatArray, output: FloatArray) {
+        val alpha = 0.05f
+
+        for (i in input.indices) {
+            output[i] = output[i] + alpha * (input[i] - output[i])
         }
     }
 }
--- app/src/main/java/com/jhiltunen/sensorlympics/magnetgame/MagnetViewModel.kt
+++ app/src/main/java/com/jhiltunen/sensorlympics/magnetgame/MagnetViewModel.kt
@@ -1,6 +1,24 @@
+import android.content.Context
+import android.util.Log
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.jhiltunen.sensorlympics.MainActivity.Companion.sensorViewModel
+import com.jhiltunen.sensorlympics.R
+import androidx.compose.runtime.livedata.observeAsState
+import com.jhiltunen.sensorlympics.ui.theme.Purple500
+import com.jhiltunen.sensorlympics.ui.theme.SensorLympicsTheme
+
+import kotlin.math.PI
+import kotlin.math.atan2
+import kotlin.random.Random
 
 class SensorViewModel : ViewModel() {
     private val _value: MutableLiveData<String> = MutableLiveData()
@@ -38,3 +56,188 @@ class SensorViewModel : ViewModel() {
         _chosen.value = chosen
     }
 }
+
+@Composable
+fun SensorApp(context: Context) {
+    val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Open))
+    var winOrLose by remember { mutableStateOf(false) }
+    var direction by remember { mutableStateOf("") }
+    //val context: Context = context
+
+    SensorLympicsTheme() {
+        Scaffold(
+            scaffoldState = scaffoldState,
+            topBar = {
+                TopAppBar(
+                    title = { Text(stringResource(R.string.app_name)) },
+                    backgroundColor = MaterialTheme.colors.primary,
+                )
+            },
+            content = {
+                Surface(color = MaterialTheme.colors.background) {
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(15.dp),
+                        elevation = 10.dp
+                    ) {
+                        Column(modifier = Modifier.padding(15.dp)) {
+                            Button(
+                                onClick = {
+                                    if (!winOrLose) {
+                                        winOrLose = true
+                                        val theChosen = sensorViewModel.chosen.value ?: 1
+                                        when (theChosen) {
+                                            1 -> direction =  context.getString(R.string.north)
+                                            2 -> direction =  context.getString(R.string.east)
+                                            3 -> direction =  context.getString(R.string.south)
+                                            4 -> direction =  context.getString(R.string.west)
+                                        }
+                                        Log.i("DIR", "Onclick chosen: $theChosen")
+                                        northOrBust(theChosen)
+
+                                    } else {
+                                        winOrLose = false
+                                        chooseDirection()
+                                        sensorViewModel.upDateWin(0)
+                                    }
+                                },
+                                modifier = Modifier.fillMaxWidth()
+                            ) {
+                                if (!winOrLose) {
+                                    val theChosen = sensorViewModel.chosen.value ?: 1
+                                    when (theChosen) {
+                                        1 -> direction =  context.getString(R.string.north)
+                                        2 -> direction =  context.getString(R.string.east)
+                                        3 -> direction =  context.getString(R.string.south)
+                                        4 -> direction =  context.getString(R.string.west)
+                                    }
+                                    Text(stringResource(R.string.btn_start, direction))
+                                } else {
+                                    Text(stringResource(R.string.btn_again))
+                                }
+                            }
+                            Spacer(Modifier.height(15.dp))
+                            ShowWinOrLose(sensorViewModel)
+                            //ShowSenorData(MainActivity.sensorViewModel)
+                            FeaturedCircularProgressIndicator(sensorViewModel)
+                        }
+                    }
+                }
+            }
+        )
+    }
+}
+
+
+
+fun northOrBust(direction: Int) {
+    when (direction) {
+        1 -> if(sensorViewModel.degree.value!! > 334 || sensorViewModel.degree.value!! < 27) {
+            sensorViewModel.upDateWin(2)
+            Log.i("DIR", "Vau!")
+        } else sensorViewModel.upDateWin(1)
+        2 -> if(sensorViewModel.degree.value!! > 64 && sensorViewModel.degree.value!! < 116) {
+            sensorViewModel.upDateWin(2)
+            Log.i("DIR", "Vau!")
+        } else sensorViewModel.upDateWin(1)
+        3 -> if(sensorViewModel.degree.value!! > 154 && sensorViewModel.degree.value!! < 206) {
+            sensorViewModel.upDateWin(2)
+            Log.i("DIR", "Vau!")
+        } else sensorViewModel.upDateWin(1)
+        4 -> if(sensorViewModel.degree.value!! > 244 && sensorViewModel.degree.value!! < 296) {
+            sensorViewModel.upDateWin(2)
+            Log.i("DIR", "Vau!")
+        } else sensorViewModel.upDateWin(1)
+        else -> sensorViewModel.upDateWin(1)
+    }
+}
+
+
+
+@Composable
+fun ShowWinOrLose(sensorViewModel: SensorViewModel) {
+    val winOrLoseOr by sensorViewModel.win.observeAsState()
+    val chosen by sensorViewModel.chosen.observeAsState()
+    val direction: String
+    when (chosen) {
+        1 -> direction =  stringResource(R.string.north)
+        2 -> direction =  stringResource(R.string.east)
+        3 -> direction =  stringResource(R.string.south)
+        4 -> direction =  stringResource(R.string.west)
+        else  -> direction =  ""
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+    ) {
+        when (winOrLoseOr) {
+            0 -> Text(stringResource(R.string.result_not_yet), Modifier.padding(8.dp))
+            1 -> Text(stringResource(R.string.result_bad), Modifier.padding(8.dp))
+            2 -> Text(stringResource(R.string.result_good, direction), Modifier.padding(8.dp))
+        }
+        Spacer(modifier = Modifier.height(7.dp))
+    }
+}
+
+@Composable
+fun FeaturedCircularProgressIndicator(sensorViewModel: SensorViewModel) {
+    val degree by sensorViewModel.degree.observeAsState()
+    Card(
+        modifier = Modifier
+            .padding(12.dp)
+            .fillMaxHeight(fraction = 0.5f)
+            .fillMaxWidth()
+            .fillMaxHeight(fraction = 0.5f),
+        elevation = 8.dp
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly,
+        ) {
+            Text((stringResource(R.string.currentMaxMin)))
+
+            Spacer(modifier = Modifier.height(7.dp))
+
+            CircularProgressIndicator(
+                progress = degree?.div(360) ?: 0.0f,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth(0.25f),
+                color = Purple500,
+                strokeWidth = 5.dp
+            )
+        }
+    }
+}
+
+fun chooseDirection() {
+    val chosen =  Random.nextInt(1, 5)
+    sensorViewModel.upDateChosen(chosen)
+    Log.i("DIR", "Chosen: $chosen")
+}
+ /*
+@Composable
+fun ShowSenorData(sensorViewModel: SensorViewModel) {
+    val value by sensorViewModel.value.observeAsState()
+    val xX by sensorViewModel.xX.observeAsState()
+    val yY by sensorViewModel.yY.observeAsState()
+    var angle = yY?.let { xX?.let { it1 -> atan2(it, it1) } }
+
+    var angleThe = (PI / 2)
+    Log.i("SENOR", value.toString())
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+    ) {
+        Text(value ?: "", Modifier.padding(8.dp))
+        Spacer(modifier = Modifier.height(7.dp))
+        Text(xX.toString() ?: "", Modifier.padding(8.dp))
+    }
+}
+*/
+
+
+
