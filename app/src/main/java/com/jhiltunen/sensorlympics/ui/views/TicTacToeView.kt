ae8d8f62098e6ed2c56af801f64ae7e506f25fd0
Joonas Hiltunen <joonas.s.jh@gmail.com>
2022-02-14 14:26:26 +0200

    improved layout

--- app/src/main/java/com/jhiltunen/sensorlympics/ui/views/TicTacToeView.kt
+++ app/src/main/java/com/jhiltunen/sensorlympics/ui/views/TicTacToeView.kt
@@ -1,16 +1,18 @@
 package com.jhiltunen.sensorlympics.ui.views
 
 import android.util.Log
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.jhiltunen.sensorlympics.utils.TicTacToe
 
 @Composable
@@ -18,14 +20,15 @@ import com.jhiltunen.sensorlympics.utils.TicTacToe
 fun TicTacToeView(ticTacToe: TicTacToe) {
     var turn = ticTacToe.turn.observeAsState()
     var gameIsOn = ticTacToe.gameIsOn.observeAsState()
-    Column() {
-        Text(text = "Turn ${turn.value}")
+    Text(text = "Turn ${turn.value}")
+
+    Row(horizontalArrangement = Arrangement.SpaceBetween) {
         for (row in 0..2) {
-            Row() {
+            Column(modifier = Modifier.padding(10.dp).height(300.dp), verticalArrangement = Arrangement.SpaceEvenly) {
                 for (column in 0..2) {
                     var btnText by remember { mutableStateOf("") }
 
-                    Button(onClick = {
+                    Button(modifier = Modifier.padding(15.dp).size(50.dp), onClick = {
                         if (gameIsOn.value == true) {
                             ticTacToe.addValue(column, row)
                             btnText = ticTacToe.situationInCoordinates(column, row)
