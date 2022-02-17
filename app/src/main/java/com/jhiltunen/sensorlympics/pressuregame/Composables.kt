b2a7d2366b9d22319de6a1ae9a619cb4b8d53c59
keenanjon <jon.menna@metropolia.fi>
2022-02-17 12:00:16 +0200

    added balloon and explosion

--- app/src/main/java/com/jhiltunen/sensorlympics/pressuregame/Composables.kt
+++ app/src/main/java/com/jhiltunen/sensorlympics/pressuregame/Composables.kt
@@ -1,19 +1,21 @@
 package com.jhiltunen.sensorlympics.pressuregame
 
+import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.R
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jhiltunen.sensorlympics.MainActivity
 import com.jhiltunen.sensorlympics.ui.theme.Purple500
 import com.jhiltunen.sensorlympics.ui.theme.SensorLympicsTheme
+import kotlin.math.round
 
 
 @Composable
@@ -30,6 +32,7 @@ fun PressureApp() {
                 Surface(color = MaterialTheme.colors.background) {
                     Card( modifier = Modifier
                         .fillMaxWidth()
+                        .fillMaxHeight()
                         .padding(15.dp),
                         elevation = 10.dp) {
                         Column(modifier = Modifier.padding(15.dp) ) {
@@ -55,14 +58,22 @@ fun FeaturedCircularProgressIndicator(pressureViewModelProgress: PressureViewMod
     val valueMax by pressureViewModelProgress.valueMax.observeAsState(null)
     val valueMin by pressureViewModelProgress.valueMin.observeAsState(null)
 
-
     val value3 = value?.minus(valueMin!!)?.div((valueMax?.minus(valueMin!!)!!))
+
+    val difference = (valueMax?.minus(valueMin!!)?.times(1000)?.let { round(it) })?.toInt()
+
+    val gameValue = (value?.minus(900)?.let { round(it) })?.toInt()
+
+    var winOrLose by remember { mutableStateOf(false) }
+
+    Log.i("PRESSURE", "?: $gameValue")
+    Log.i("PRESSURE", "??: $difference")
 
     Card(modifier = Modifier
         .padding(12.dp)
-        .fillMaxHeight(fraction = 0.5f)
+        .fillMaxHeight(fraction = 0.95f)
         .fillMaxWidth()
-        .fillMaxHeight(fraction = 0.5f)
+        .fillMaxHeight(fraction = 1.75f)
         ,
         elevation = 8.dp
     ) {
@@ -70,8 +81,40 @@ fun FeaturedCircularProgressIndicator(pressureViewModelProgress: PressureViewMod
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.SpaceEvenly,
         ) {
-
+            Button (
+                modifier = Modifier
+                    .fillMaxWidth(),
+                onClick = {
+                    winOrLose = !winOrLose
+                }
+            ) {
+                if(!winOrLose) {
+                    Text("Paina perkele paina!")
+                } else {
+                    Text("Älä saatana paina!")
+                }
+            }
             Text(stringResource(com.jhiltunen.sensorlympics.R.string.currentMaxMin))
+            if (difference != null) {
+                if (difference < 180) {
+                    Text(
+                        "\uD83C\uDF88",
+                        fontSize = difference.sp
+                    )
+                } else {
+                    Text(
+                        "\uD83D\uDCA5",
+                        fontSize = 180.sp
+                    )
+                }
+            } else {
+                Text(
+                    "\uD83C\uDF88",
+                    fontSize = 60.sp
+                )
+            }
+
+
 
             Spacer(modifier = Modifier.height(7.dp))
 
