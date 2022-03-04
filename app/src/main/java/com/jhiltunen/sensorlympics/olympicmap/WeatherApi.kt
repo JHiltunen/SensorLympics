e7a43916c05ece8f4f2656bf39232757268caa9a
Jon Menna <75067106+keenanjon@users.noreply.github.com>
2022-03-04 08:11:12 +0200

    Update WeatherApi.kt

--- app/src/main/java/com/jhiltunen/sensorlympics/olympicmap/WeatherApi.kt
+++ app/src/main/java/com/jhiltunen/sensorlympics/olympicmap/WeatherApi.kt
@@ -17,7 +17,7 @@ import retrofit2.http.Query
 
 object WeatherApi {
     const val URL = "https://api.openweathermap.org/"
-    const val appid = "4020606b3cef7fbdc80715c2056ad6cf"
+    const val appid = ""
 
     object Model {
         data class All (val main: Main)
