b293ce52548682c8c09b28ffaae5c3c274923ddc
Joonas Hiltunen <joonas.s.jh@gmail.com>
2022-03-08 17:38:55 +0200

    Added rectangle as win surface

--- app/src/main/java/com/jhiltunen/sensorlympics/ui/views/BallGameView.kt
+++ app/src/main/java/com/jhiltunen/sensorlympics/ui/views/BallGameView.kt
@@ -1,7 +1,12 @@
 package com.jhiltunen.sensorlympics.ui.views
 
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Card
@@ -12,19 +17,37 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import com.jhiltunen.sensorlympics.CardStyle
 import com.jhiltunen.sensorlympics.MainActivity.Companion.ballGameViewModel
+import com.jhiltunen.sensorlympics.R
+import kotlin.random.Random
 
 @ExperimentalFoundationApi
 @Composable
 fun BallGameView() {
     val xPosition by ballGameViewModel.xPosition.observeAsState()
     val yPosition by ballGameViewModel.yPosition.observeAsState()
+    //(0..ballGameViewModel.xMax.toInt()).random().toFloat(), (0..ballGameViewModel.yMax.toInt()).random().toFloat()
+
+    val option = BitmapFactory.Options()
+    option.inPreferredConfig = Bitmap.Config.ARGB_8888
+    val bitmap = BitmapFactory.decodeResource(
+        LocalContext.current.resources,
+        R.drawable.img,
+        option
+    ).asImageBitmap()
 
     Surface(color = MaterialTheme.colors.background) {
         Card {
             CardStyle {
+                var xRandom: Float = 0f
+                var yRandom: Float = 0f
                 Canvas(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -32,6 +55,8 @@ fun BallGameView() {
                 ) {
                     if (ballGameViewModel.xMax != size.width && ballGameViewModel.yMax != size.height) {
                         ballGameViewModel.setMaxValues(size.width, size.height)
+                        xRandom = (0..ballGameViewModel.xMax.toInt()).random().toFloat()
+                        yRandom = (0..ballGameViewModel.xMax.toInt()).random().toFloat()
                     }
 
                     if (xPosition!! <= 0 || xPosition!! >= ballGameViewModel.xMax) {
@@ -49,6 +74,13 @@ fun BallGameView() {
                             y = yPosition!!
                         )
                     )
+
+                    /*drawImage(
+                        image = bitmap,
+                        topLeft = Offset(randomX, randomY)
+                    )*/
+
+                    drawRect(Color.Blue, topLeft = Offset(xRandom, yRandom), size = Size(200f, 200f))
                 }
             }
         }
